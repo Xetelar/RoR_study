0537598e54eaa63650c931c4b1c5eeacb659408a
Xetelar <earlvrotten@ya.ru>
2018-04-13 15:39:03 +0300

    homework 4 fix

---
+++ homework_4/train.rb
@@ -0,0 +1,73 @@
+class Train
+  attr_reader :speed, :cars
+
+  def initialize(number, type)
+    @number = number
+    @type = type
+    @cars = []
+    @speed = 0
+    @station_index = 0
+  end
+
+  def accelerate(value)
+    @speed += value if value > 0
+  end
+
+  def stop(value)
+    @speed = @speed > 0 ? @speed - value : 0
+  end
+
+  def add_car(car)
+    @cars << car if @speed.zero? && @type == car.type
+  end
+
+  def remove_car(car_number)
+    @cars.delete_at(car_number) if speed.zero? && !@cars.empty?
+  end
+
+  def add_route(route)
+    @route = route
+    @station_index = 0
+    current_station.add_train(self)
+  end
+
+  def move_back
+    unless first_station?
+      current_station.send_train(self)
+      @station_index -= 1
+      current_station.add_train(self)
+    end
+  end
+
+  def move_next
+    unless last_station?
+      current_station.send_train(self)
+      @station_index += 1
+      current_station.add_train(self)
+    end
+  end
+
+  def next_station
+    @route.stations[@station_index + 1]
+  end
+
+  def current_station
+    @route.stations[@station_index]
+  end
+
+  def prev_station
+    @route.stations[@station_index - 1] if @station_index > 0
+  end
+
+  def last_car_type?
+    @cars[-1].type
+  end
+
+  def last_station?
+    current_station == @route.stations[-1]
+  end
+
+  def first_station?
+    current_station == @route.stations[0]
+  end
+end
